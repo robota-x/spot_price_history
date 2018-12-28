7e1112b27437c5062cc98487c1157b283c106b54
Lorenzo Turrino <neuronefunzionante@gmail.com>
2018-12-28 11:08:03 +0000

    add s3 upload to script

--- deploy.sh
+++ deploy.sh
@@ -22,4 +22,11 @@ aws lambda update-function-code --profile spot --function-name spot_orchestrator
 aws lambda update-function-code --profile spot --function-name spot_parser --zip-file fileb://deploy_packages/parser.zip
 aws lambda update-function-code --profile spot --function-name spot_writer --zip-file fileb://deploy_packages/writer.zip
 
+# upload to s3
+if [ ! -z "$LIST_TARGET" ] 
+then 
+    echo 'uploading requirements list to s3...'
+    aws s3 cp --profile spot required_data.json s3://$LIST_TARGET
+fi
+
 echo 'all done!'

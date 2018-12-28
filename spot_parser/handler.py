678cb50c9a865fff3778ffbbbd4bbb539d771a2e
Lorenzo Turrino <neuronefunzionante@gmail.com>
2018-12-28 12:31:56 +0000

    make parser compatible for all regions, change to accept region instead of availability zone

--- spot_parser/handler.py
+++ spot_parser/handler.py
@@ -7,69 +7,66 @@ from datetime import datetime, timedelta
 from dateutil.parser import parse
 
 
-ec2_client = boto3.client('ec2')
-lambda_client = boto3.client('lambda')
-
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 
 def spot_parser(spot):
-    spot['Timestamp'] = spot['Timestamp'].isoformat(
-    ) if 'Timestamp' in spot else None
+    spot['Timestamp'] = spot['Timestamp'].isoformat() if 'Timestamp' in spot else None
 
     return spot
 
 
-def price_grabber(availability_zone, instance_types, end_time=None, start_time=None):
+def get_availability_zones(client, region_name):
+    res = client.describe_availability_zones()
+
+    return [zone.get('ZoneName') for zone in res.get('AvailabilityZones', [])]
+
+
+def get_region_spot_prices(region, instance_types, end_time=None, start_time=None):
+    client = boto3.client('ec2', region_name=region)
+    availability_zones = get_availability_zones(client, region)
+
     computed_end_time = end_time or datetime.now()
-    computed_start_time = start_time or (
-        computed_end_time - timedelta(seconds=3610))
+    computed_start_time = start_time or (computed_end_time - timedelta(seconds=3610))
 
+    spot_prices = []
     # not using filters as they seem to be quite flaky results
-    res = ec2_client.describe_spot_price_history(
-        AvailabilityZone=availability_zone,
-        InstanceTypes=instance_types,
-        EndTime=computed_end_time,
-        StartTime=computed_start_time,
-        ProductDescriptions=['Linux/UNIX', 'Windows'],
-    )
+    for zone in availability_zones:
+        res = client.describe_spot_price_history(
+            AvailabilityZone=zone,
+            InstanceTypes=instance_types,
+            EndTime=computed_end_time,
+            StartTime=computed_start_time,
+            ProductDescriptions=['Linux/UNIX', 'Windows'],
+        )
+        zone_prices = [spot_parser(spot) for spot in res.get('SpotPriceHistory', [])]
 
-    return [spot_parser(spot) for spot in res.get('SpotPriceHistory', [])]
+        print(f'fetched {len(zone_prices)} prices for zone: {zone}')
+        spot_prices += zone_prices
+
+    return spot_prices
 
 
 def lambda_handler(event, context):
     try:
-        availability_zone = event.get('availability_zone', 'eu-west-1a')
+        region = event.get('region', 'eu-west-1')
         instance_types = event.get('instance_types', ['t1.micro'])
 
         end_time = parse(event['end_time']) if 'end_time' in event else None
-        start_time = parse(event['start_time']
-                           ) if 'start_time' in event else None
+        start_time = parse(event['start_time']) if 'start_time' in event else None
 
-        spot_prices = price_grabber(
-            availability_zone,
-            instance_types,
-            end_time=end_time,
-            start_time=start_time
-        )
+        spot_prices = get_region_spot_prices(region, instance_types, end_time=end_time, start_time=start_time)
 
         if(len(spot_prices)):
-            lambda_client.invoke(
+            boto3.client('lambda').invoke(
                 FunctionName=config['aws']['writer_function'],
                 InvocationType='Event',
                 Payload=json.dumps({'spot_prices': spot_prices})
             )
 
-        print(
-            f'parser processed {len(spot_prices)} entries for zone: {availability_zone}')
-        return {
-            'statusCode': 200,
-            'body': {
-                'processed_entries': len(spot_prices)
-            }
-        }
+        print(f'parser processed {len(spot_prices)} entries for region: {region}')
     except:
         # dump trace and event
-    traceback.print_exc()
-    print(json.dumps(event))
+        traceback.print_exc()
+        print(json.dumps(event))

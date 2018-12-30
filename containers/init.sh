369f222d17b058554d13cc180a225cc5bced1cd9
Lorenzo Turrino <neuronefunzionante@gmail.com>
2018-12-30 19:29:35 +0000

    tweak runner and psql docker commands

--- containers/init.sh
+++ containers/init.sh
@@ -4,5 +4,11 @@
 mkdir -p data/psql
 
 # run psql server
-export DB_DOCKER_ID=$(sudo docker run --rm --name postgres-server -d -p 15432:5432 -v $(pwd)/data/psql:/var/lib/postgresql/data postgres:alpine)
+export DB_DOCKER_ID=$(sudo docker run --rm --name postgres-server -d -v $(pwd)/data/psql:/var/lib/postgresql/data postgres:alpine)
 echo started container $DB_DOCKER_ID
+
+# run data fetcher
+sudo docker run --rm --network spot_monitor --name spot-runner -e ENV=dev -v ~/.aws:/.aws  spot_runner:v1
+
+# admin version with published port to localhost
+# sudo docker run --rm --network spot_monitor --name psql-server -d -p 15432:5432 -v $(pwd)/data/psql:/var/lib/postgresql/data postgres:alpine 

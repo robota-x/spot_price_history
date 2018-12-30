98c31a35a2e91f8f3b43a54d649c131ff2596f7f
Lorenzo Turrino <neuronefunzionante@gmail.com>
2018-12-30 22:33:52 +0000

    trial sample docker-compose file

---
+++ containers/compose-runner.sh
@@ -0,0 +1,12 @@
+# usage: > sudo bash compose-runner.sh dev
+
+# set up env vars for compose interpolation
+export PSQL_MOUNT=$(pwd)
+export ENV=${1:-live}
+
+if [ $ENV = 'live' ]; 
+    then export RUNNER_TAG=v1; 
+    else export RUNNER_TAG=dev; 
+fi
+
+docker-compose up
